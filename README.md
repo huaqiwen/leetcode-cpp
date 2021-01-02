77ab0a160e1ba3799e793963457ece9092340c82
Qiwen Hua <hqw@huaqiwen.com>
2021-01-01 16:07:15 -0800

    633. Sum of Square Numbers

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ Leetcode solutions in C++.
 - [76. Minimum Window Substring](double-pointers/76-min-window-substring.cpp)
 - [142. Linked List Cycle II](double-pointers/142-linked-list-cycle-ii.cpp)
 - [167. Two Sum II - Input array is sorted](double-pointers/167-two-sum-ii.cpp)
+- [633. Sum of Square Numbers](double-pointers/633-sum-square-numbers.cpp)
---
+++ double-pointers/633-sum-square-numbers.cpp
@@ -0,0 +1,33 @@
+// ID: 76
+// Title: Minimum Windows Substring
+// Type: Two Pointers
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/minimum-window-substring/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool judgeSquareSum(int c);
+
+int main() {
+    cout << (judgeSquareSum(5) ? "true" : "false") << endl;
+
+    return 0;
+}
+
+bool judgeSquareSum(int c) {
+    int l = 0, r = floor(sqrt(c));
+
+    while (l <= r) {
+        // comparing c^2 - b^2 with a^2 instead of a^2 + b^2 with c^2 to avoid integer overflow and using long long
+        int lsqr = l * l, rsqr = r * r;
+        if (c - lsqr == rsqr) return true;
+
+        if (c - lsqr < rsqr) r--;
+        else l++;
+    }
+
+    return false;
+}
