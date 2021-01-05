3e77c7e5e4b2193302f3f54865151467d4072175
Qiwen Hua <hqw@huaqiwen.com>
2021-01-05 10:55:18 -0800

    139. Word Break

--- dynamic-programming/139-word-break.cpp
+++ dynamic-programming/139-word-break.cpp
@@ -20,13 +20,16 @@ int main() {
 
 bool wordBreak(string s, vector<string>& wordDict) {
     int n = s.size();
+    // dp stores whether the substring ending at index can be broken into dict words
     vector<bool> dp(n, false);
 
     for (int i = 0; i < n; i++) {
         for (string word : wordDict) {
             int word_len = word.size();
+            // word size == curr str size AND word == curr str
             if (word_len == i + 1 && s.substr(i - word_len + 1, word_len) == word)
                 dp[i] = true;
+            // word size < curr str size AND word == last part of curr str AND the str before the word can be broken
             if (word_len < i + 1 && s.substr(i - word_len + 1, word_len) == word && dp[i - word_len])
                 dp[i] = true;
         }
