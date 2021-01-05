913749e56d2f275ac7e4cd7147061ecb864601c8
Qiwen Hua <hqw@huaqiwen.com>
2021-01-05 13:18:14 -0800

    300. Longest Increasing Subsequence

--- dynamic-programming/300-longest-increasing-subseq.cpp
+++ dynamic-programming/300-longest-increasing-subseq.cpp
@@ -10,14 +10,19 @@
 using namespace std;
 
 int lengthOfLIS(vector<int>& nums);
+int lengthOfLIS2(vector<int>& nums);
 
 int main() {
     vector<int> in = {10, 9, 2, 5, 3, 7, 101, 18};
-    cout << lengthOfLIS(in) << endl;
+    cout << lengthOfLIS2(in) << endl;
 }
 
+/**
+ * @brief Brute force dp, O(n^2)
+ */
 int lengthOfLIS(vector<int>& nums) {
     int n = nums.size(), max_len = 1;
+    // dp stores the length of the LIS ending at nums[index]
     vector<int> dp(n, 1);
 
     for (int i = 0; i < n; i++) {
@@ -28,4 +33,25 @@ int lengthOfLIS(vector<int>& nums) {
     }
 
     return max_len;
+}
+
+/**
+ * @brief Better dp, O(nlogn)
+ */
+int lengthOfLIS2(vector<int>& nums) {
+    int n = nums.size();
+    if (n <= 1) return n;
+    // dp stores the smallest number in nums that can be the end of a IS with length index + 1
+    vector<int> dp = {nums[0]};
+
+    for (int i = 1; i < n; i++) {
+        if (nums[i] > dp.back()) {
+            dp.push_back(nums[i]); 
+        } else {
+            auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
+            *it = nums[i];
+        }
+    }
+
+    return dp.size();
 }
