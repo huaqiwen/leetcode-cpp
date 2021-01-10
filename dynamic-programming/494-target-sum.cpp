7402aa3640f860fb8718e3620a88941469071345
Qiwen Hua <hqw@huaqiwen.com>
2021-01-09 16:49:55 -0800

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ dynamic-programming/494-target-sum.cpp
@@ -0,0 +1,21 @@
+// ID: 494
+// Title: Target Sum
+// Type: Dynamic Programming
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/target-sum/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findTargetSumWays(vector<int>& nums, int S);
+
+int main() {
+    vector<int> in = {1, 1, 1, 1, 1};
+    cout << findTargetSumWays(in, 3) << endl;
+}
+
+int findTargetSumWays(vector<int>& nums, int S) {
+    
+}
