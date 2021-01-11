dd8a59043db3c338de629756a0a5046789fd6bed
Qiwen Hua <hqw@huaqiwen.com>
2021-01-10 23:05:23 -0800

    62. Unique Paths

--- README.md
+++ README.md
@@ -68,6 +68,7 @@ Leetcode solutions in C++.
 - [213. House Robber II](dynamic-programming/213-house-robber-ii.cpp)
 - [413. Arithmetic Slices](dynamic-programming/413-arithmetic-slices.cpp)
 ### 2D Basic <a name="dp-2d">
+- [62. Unique Paths](dynamic-programming/62-unique-paths.cpp)
 - [64. Minimum Path Sum](dynamic-programming/64-min-path-sum.cpp)
 - [221. Maximal Square](dynamic-programming/221-max-square.cpp)
 - [542. 01 Matrix](dynamic-programming/542-zero-one-matrix.cpp)
---
+++ dynamic-programming/62-unique-paths.cpp
@@ -0,0 +1,37 @@
+// ID: 62
+// Title: Unique Paths
+// Type: Dynamic Programming
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/unique-paths/
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int uniquePaths(int m, int n);
+
+int main() {
+    cout << uniquePaths(3, 7) << endl;
+}
+
+int uniquePaths(int m, int n) {
+    // dp[i][j] denotes # unique paths from top-left to grid[i][j]
+    // TODO: since we only use the values from the last "layer", we can compress dp array to 1D
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    dp[0][0] = 1;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i > 0) {
+                dp[i][j] += dp[i-1][j];
+            }
+            if (j > 0) {
+                dp[i][j] += dp[i][j-1];
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
+}
